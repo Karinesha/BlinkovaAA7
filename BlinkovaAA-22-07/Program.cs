ce2c8c3e7b26cc17cd91370095e5cbd231ab139b
Karinesha <karinablinkova25@mail.ru>
2023-09-15 00:01:35 +0300

    туц

---
+++ BlinkovaAA-22-07/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace BlinkovaAA_22_07;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Введите число");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Вы ввели: {name}!");
+    }
+}
